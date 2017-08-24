75beef3c7788b4446e5d453d9078459549f2b548
Thejoker2012 <31083428+Thejoker2012@users.noreply.github.com>
2017-08-24 17:02:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-void main()
-
-  {
+int main()
+{
+  // <= Use // para fazer um comentário nos codigos,e aceita comentario em apenas uma linha
+  
+  /* Use barra asterisco para comentario muito longo */
+  
+  /n - informa ao compilador para pular uma linha
+   
+   
+ 
   
   
- }
+    printf ("Aula 1.\n);
+            
+    
+    return 0;       
+}
